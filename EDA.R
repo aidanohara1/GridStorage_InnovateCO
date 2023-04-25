97580e7c1d342f8df9d443369ad4c779c22925ae
aidanohara1 <aohara1@bu.edu>
2023-04-24 23:12:51 -0400

    init commit

---
+++ EDA.R
@@ -0,0 +1,6 @@
+
+
+revByutility <- read.csv("Electricity_Revenue_by_Utility_in_Colorado.csv")
+View(revByutility)
+# https://data.colorado.gov/Business/Electricity-Revenue-by-Utility-in-Colorado/gdh8-8pg4
+
